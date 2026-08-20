71079bc56b1ad009c7f99c8823ebcee41f1a906c
ROHAN BINWADE <rohanpython9229@gmail.com>
2026-08-20 11:58:42 +0530

    Fix API routing for single URL deployment

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 uvicorn api.main:app --host 0.0.0.0 --port 8000 &
 
-streamlit run app.py 
-  --server.address=0.0.0.0 
-  --server.port=${PORT:-8501} 
+streamlit run app.py \
+  --server.address=0.0.0.0 \
+  --server.port=${PORT:-8501} \
   --server.fileWatcherType=none
